5709f6cd9d5edaab90112aa753d13113a2d40057
Kist221 <Kist221@gmail.com>
2018-02-09 22:42:01 -0700

    done

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,7 +3,7 @@ var friendsArray = [
 	{
 	name:"Steve",
 	photo:"https://www.wired.com/images_blogs/business/2011/08/steve_jobs.jpg",
-	scores:["1", "1", "1", "1", "1", "4", "4", "4", "4", "3"]
+	scores:["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
 	}
 ];
 
